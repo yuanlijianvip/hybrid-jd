f77928d776810024b06b8dd0aebba6fd76c653ec
yuanlijian <yuanlijianvip@163.com>
2020-05-23 01:18:52 +0800

    init

---
+++ hybrid-jd-web/src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <!-- <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <div class="test">测试Test</div>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+@import '@css/style.scss';
+
+.test {
+  font-size: px2rem(16);
+}
+
+</style>
